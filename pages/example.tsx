1c3538c1520b9cd11d82f5e82f70e08fa5295d0f
Austin Whitbeck <AustinWhitbeckArt@gmail.com>
2023-06-11 05:10:18 -0400

    Removed global styling, added example of /pages directory and auth.js

---
+++ pages/example.tsx
@@ -0,0 +1,34 @@
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import { prisma } from '../lib/prisma';
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+      const user = await prisma.user.findFirst({
+    where: {
+      email: `test@test.com`,
+    }
+  })
+    return({
+        props: {
+            user
+        }
+    }
+    )
+}
+
+
+type ExamplePageProps = InferGetServerSidePropsType<typeof getServerSideProps>
+
+const ExamplePage = ({ user }: ExamplePageProps) => {
+
+      const userRenderResult = () => {
+    if (user) {
+      return <h1> Hello {user.name} !</h1>
+    }
+    return <h1> No user Found</h1>
+  };
+  return (
+    <div>{userRenderResult()}</div>
+  )
+}
+
+export default ExamplePage;
